0de2608d62c211b4c4068d3e108ef05ba0f78cd5
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-04 13:24:09 -0500

    starting to add routes and a controller

--- db/seed.js
+++ db/seed.js
@@ -20,22 +20,3 @@ fishingSpots.deleteMany({}).then(() => {
       console.log(err);
     });
 });
-
-// const countryData = data.map(item => {
-//   const country = {}
-//   country.name = item.name
-//   country.capital = item.capital
-//   country.region = item.region
-//   country.population = item.population
-//   return country
-// })
-// Country.remove({})
-//     .then(() => {
-//         Country.create(countryData)
-//             .then(countries => {
-//                 console.log(countries)
-//             })
-//             .catch(err => {
-//                 console.log(err)
-//             })
-//     })
