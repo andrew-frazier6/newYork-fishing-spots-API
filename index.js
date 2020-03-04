ad551e957972e43dc6f0015d263e1e1fadcd9678
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-04 16:17:02 -0500

    removed routes, not needed. pushing get request through index

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ const fishingSpot = require("./db/modules/fishingSpot");
 app.get("/", function(req, res) {
   fishingSpot.find({}).then(fishingData => res.json(fishingData));
 });
+app.get("/name", function(req, res) {
+  fishingSpot.find({ name: req.params.name }).then(fishingSpot => {
+    res.json(fishingSpot);
+  });
+});
+
+//   (fishingSpots.name = item.name),
+//     (fishingSpots.fish_types = item.fish_spec),
+//     (fishingSpots.public_access = item.public_acc),
+//     (fishingSpots.location = item.location);
+
 app.listen(3000, () => console.log("listening on port 3000"));
