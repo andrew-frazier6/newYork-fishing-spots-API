c4c043d68b6bb6747ab726a668078c367f1f965d
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-05 16:18:45 -0500

    update for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
+let mongoURI = "";
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true
 });
-let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/ny_fishing_spots";
 }
-
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(instance =>
