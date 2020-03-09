a038bdf2ab6b648836d9d4dbfdf33713050b7cf6
Andrew Frazier <acorbettfrazier@gmail.com>
2020-03-09 00:06:14 -0400

    fixed CORS issue

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const fishingSpot = require("./db/modules/fishingSpot");
 const parser = require("body-parser");
 
 app.use(parser.json());
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
+    return res.send(204);
+  }
+  next();
+});
+
 app.get("/", function(req, res) {
   fishingSpot.find({}).then(fishingData => res.json(fishingData));
 });
